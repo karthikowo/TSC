5519d83b1e00b2e8afc2d43eaf353294c637959a
karthikowo <20pw16@psgtech.ac.in>
2023-03-11 09:22:10 +0530

    Setup Endpoints

--- api_endpoints.py
+++ api_endpoints.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
+app = FastAPI()
+
+@app.get("/")
+def root():
+    dummy_data = {"word":"Hello World"}
+    return dummy_data
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
