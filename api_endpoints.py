168f5e9dd0aff5431bcee710fcf970a261c59d1f
karthikowo <20pw16@psgtech.ac.in>
2023-03-11 10:22:31 +0530

    Accept CSV files via uploadFile

--- api_endpoints.py
+++ api_endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+import pandas as pd
 app = FastAPI()
 
 @app.get("/")
@@ -8,4 +9,7 @@ def root():
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+    df = pd.read_csv(file.file)
+    mape = model_train(df)
+
+    return {"mape":mape,"filename": file.filename}
